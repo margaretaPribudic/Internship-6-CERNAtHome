61a857cea16a0ea272004c15df61aa093547045e
margaretaPribudic <pribudicmargareta@gmail.com>
2022-12-19 21:31:45 +0100

    New database tables

---
+++ CERNAtHomeNewTables.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Accelerators(
+	AcceleratorId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Projects(
+	ProjectId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE AcceleratorsProjects(
+	AcceleratorId INT REFERENCES Accelerators(AcceleratorId),
+	ProjectId INT REFERENCES Projects(ProjectId),
+	PRIMARY KEY(AcceleratorId,ProjectId)
+)
+
+CREATE TABLE ScientificWorks(
+	ScientificWork SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL,
+	DateOfPublication TIMESTAMP,
+	NumberOfQuotes INT,
+	ProjectId INT REFERENCES Projects(ProjectId)
+)
+
+CREATE TABLE Professions(
+	ProfessionId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE Cities(
+	CityId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL
+)
+
+CREATE TABLE States(
+	StateId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL,
+	Population INT,
+	PPPPerCapita FLOAT
+)
+
+CREATE TABLE StatesCities(
+	StateCityId SERIAL PRIMARY KEY,
+	StateId INT REFERENCES States(StateId),
+	CityId INT REFERENCES Cities(CityId)
+)
+
+CREATE TABLE Hotels(
+	HotelId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL,
+	StateCityId INT REFERENCES StatesCities(StateCityId)
+)
+
+CREATE TABLE Scientists(
+	ScientistId SERIAL PRIMARY KEY,
+	Name VARCHAR(30) NOT NULL,
+	BirthDate TIMESTAMP,
+	Sex VARCHAR(30),
+	StateId INT REFERENCES States(StateId),
+	ProfessionId INT REFERENCES Professions(ProfessionId),
+	HotelId INT REFERENCES Hotels(HotelId)
+)
+
+CREATE TABLE ScientistsScientificWork(
+	ScientistId INT REFERENCES Scientists(ScientistId),
+	ScientificWorkId INT REFERENCES ScientificWorks(ScientificWork),
+	PRIMARY KEY(ScientistId,ScientificWorkId)
+)
+
